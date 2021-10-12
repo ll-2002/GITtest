417ac8a729b4e216c65705e8240fd61b4c77036f
hhh <1540282804@qq.com>
2021-10-12 20:21:38 +0800

    IDEAwo

---
+++ src/main/java/work1/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package work1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+    @RequestMapping
+    public String hello() {
+        return "helloword Spring Boot！这是一个用Spring Boot开发的网站。";
+    }
+
+}
